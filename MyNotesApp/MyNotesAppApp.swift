1724af816a9f38804a09c06ca9cf7dd9aeef65fd
Martin <marrty.h.1992@gmail.com>
2026-01-03 18:05:41 +0100

    Initial Commit

---
+++ MyNotesApp/MyNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesAppApp.swift
+//  MyNotesApp
+//
+//  Created by Martin Hrbáček on 03.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
